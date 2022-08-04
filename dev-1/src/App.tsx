d7c574b79760feccc8a04182bc103ab39c4acc43
FengY1ng <DrFengY1ng@gmail.com>
2022-08-04 22:48:36 +0800

    转为React

---
+++ dev-1/src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './dev-1.css';
+
+
+function App() {
+  return (
+    <div className="All">
+        <h2>登录</h2>
+        <div className="Login">
+            <div className="input-box">
+                <label>账号:</label>
+                <input type="text"/>
+            </div>
+            <div className="input-box">
+                <label>密码:</label>
+                <input type="password"/>
+            </div>
+            <div className="btn-box">
+                <a href="#">忘记密码？</a>
+                <div>
+                    <button className='login'>登录</button>
+                    <button className='registered'>注册</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ dev-1/src/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function login() {
+  return (
+    <script>
+        let login=document.querySelector('login');
+        login.onclick()=function(){
+            
+        }
+    </script>
+  )
+}
+
+export default login
